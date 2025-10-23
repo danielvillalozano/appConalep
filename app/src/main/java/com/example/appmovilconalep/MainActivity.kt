57e1d7a7136a71b841bdb9e0514c7bef7c10b0a2
Daniel Villa lozano <danielvillalozano6@gmail.com>
2025-10-23 12:42:26 -0700

    Se creo MainActivity y layout basico para acceso al sistema

--- app/src/main/java/com/example/appmovilconalep/MainActivity.kt
+++ app/src/main/java/com/example/appmovilconalep/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.appmovilconalep
 
+package com.example.controlconalep
+
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnEntrar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnEntrar = findViewById(R.id.btnEntrar)
+
+        btnEntrar.setOnClickListener {
+            val intent = Intent(this, GruposActivity::class.java)
+            startActivity(intent)
         }
     }
 }
