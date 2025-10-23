accb44a95acb7c64a9f2636cd664880e0162a0a5
Daniel Villa lozano <danielvillalozano6@gmail.com>
2025-10-23 12:34:26 -0700

    se creo la tabla definetabla con estructura de grupo, alumno y asistencia

--- app/src/main/java/com/example/appmovilconalep/DefineTabla.kt
+++ app/src/main/java/com/example/appmovilconalep/DefineTabla.kt
@@ -1,4 +1,27 @@
-package com.example.appmovilconalep
+package com.example.appmovilconalep.basedatos
 
-class DefineTabla {
+object DefineTabla {
+
+    object Grupo {
+        const val NOMBRE_TABLA = "grupo"
+        const val COLUMNA_ID = "id"
+        const val COLUMNA_NOMBRE = "nombre"
+    }
+
+    object Alumno {
+        const val NOMBRE_TABLA = "alumno"
+        const val COLUMNA_ID = "id"
+        const val COLUMNA_NOMBRE = "nombre"
+        const val COLUMNA_MATRICULA = "matricula"
+        const val COLUMNA_ID_GRUPO = "idGrupo"
+    }
+
+    object Asistencia {
+        const val NOMBRE_TABLA = "asistencia"
+        const val COLUMNA_ID = "id"
+        const val COLUMNA_ID_ALUMNO = "idAlumno"
+        const val COLUMNA_FECHA = "fecha"
+        const val COLUMNA_ASISTIO = "asistio"
+        const val COLUMNA_JUSTIFICADA = "justificada"
+    }
 }
