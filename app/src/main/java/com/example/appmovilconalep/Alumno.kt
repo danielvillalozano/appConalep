efa206a06d2a7565db6ed30809a22bef6ba77051
Daniel Villa lozano <danielvillalozano6@gmail.com>
2025-10-23 07:39:04 -0700

    Se genero la clase alumno

---
+++ app/src/main/java/com/example/appmovilconalep/Alumno.kt
@@ -0,0 +1,8 @@
+package com.example.appmovilconalep
+
+data class Alumno(
+    var id: Int = 0,
+    var nombre: String,
+    var matricula: String,
+    var idGrupo: Int
+)
---
+++ app/src/main/java/com/example/appmovilconalep/Asistencia.kt
@@ -0,0 +1,4 @@
+package com.example.appmovilconalep
+
+class Asistencia {
+}
